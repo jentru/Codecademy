0fc9e46d7641cad104fd5f1483afaa1938d81c44
jentru <114699983+jentru@users.noreply.github.com>
2023-02-15 17:14:37 -0600

    Create magic-eight-ball.js

---
+++ magic-eight-ball.js
@@ -0,0 +1,39 @@
+//user name greeting
+let userName ='';
+userName? console.log(`Hello, ${userName}!`): console.log('Hello!');
+
+//input a question and display on console
+let userQuestion = 'Am I able to code?';
+console.log(userQuestion);
+
+let randomNumber = Math.floor(Math.random()*8);
+let eightBall = '';
+
+//random outcomes are based on the random number generated
+switch (randomNumber){
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
+console.log(eightBall);
